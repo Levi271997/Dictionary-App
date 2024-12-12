a250ba7cb27d11d0dea8a9e06c500e696bc499e2
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2024-12-13 01:10:17 +0800

    functioning theme switcher

--- src/App.js
+++ src/App.js
@@ -50,10 +50,17 @@ export {FontFamToggle}
 
 
 const ThemeSwitcher=()=>{
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+    document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
+  };
+
   return(
    <div className='switcher-hold'>
       <div className='switcher'>
-        <input type='checkbox'/>
+        <input type='checkbox' checked={isDark} onChange={toggleTheme}/>
         <div className='pointer'></div>
       </div>
       <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
